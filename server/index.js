cb7b251136a0fea34b9eeefa5b6053c70fa49e1f
Denis Solovjov <denekyn@gmail.com>
2021-05-18 18:53:02 +0300

    added: build for production

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import animalRoutes from "./routes/animals.js";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/animals", animalRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  console.log(process.env.BASE_URL);
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+  console.log(path.join(__dirname, "../client/build/index.html"));
+}
 
 async function start() {
   try {
